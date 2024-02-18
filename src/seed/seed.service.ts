dd446810e3d951b7a4c869bfee763dd9ff0c4b65
Daniela Carballo <daniela.carballo@biggertech.co>
2024-02-18 00:09:47 -0300

    Add multiple insertions into the db

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import axios, { AxiosInstance } from 'axios';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      console.log({ name, no });
+      // await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no }); // [{ name: bulbasaur, no: 1 }]
     });
 
-    return data.results;
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed';
   }
 }
